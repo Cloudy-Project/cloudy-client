500dbeeabe6aec4734c6997b1999b4f1205f55cd
JuyeonO <yexnx01@gmail.com>
2024-07-25 08:48:59 +0900

    chore: 피드백 기반 수정-4

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import '../css/HeaderContainer.css';
 
-export default function HeaderContainer({ username = ' ' }) {
-  const getConsonant = (name) => {
-    const lastChar = name.charCodeAt(name.length - 1);
+export default function HeaderContainer({ name = '이름' }) {
+  const Unicode_가 = 0xAC00;
+  const Unicode_힣 = 0xD7A3;
 
-    if (lastChar >= 0xAC00 && lastChar <= 0xD7A3) {
+  const getConsonant = (inputName) => {
+    const lastChar = inputName.charCodeAt(inputName.length - 1);
+
+    if (lastChar >= Unicode_가 && lastChar <= Unicode_힣) {
       const consonantCode = (lastChar - 44032) % 28;
       return consonantCode === 0 ? '의' : '이의';
     }
+
+    //한글 이외의 경우 name+'의' 편지함 반환
     return '의';
   };
 
-  const consonant = getConsonant(username);
+  const consonant = getConsonant(name);
 
   return (
     <div className='header-container'>
-      <label className='user-text'>{username}{consonant} 편지함</label>
+      <label className='user-text'>{name}{consonant} 편지함</label>
     </div>
   );
 }
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -2,5 +2,7 @@ import React from 'react';
 import HeaderContainer from '../components/HeaderContainer';
 
 export default function MyPage() {
-  return <HeaderContainer></HeaderContainer>;
+  return (
+    <HeaderContainer></HeaderContainer>
+  );
 }
