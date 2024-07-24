7a29bdaef465c3a8511a021768d76fe81e8adc09
JuyeonO <yexnx01@gmail.com>
2024-07-24 20:20:26 +0900

    chore: 피드백 기반 수정-3

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import '../css/HeaderContainer.css';
 
-export default function HeaderContainer({ username }) {
+export default function HeaderContainer({ username = ' ' }) {
+  const getConsonant = (name) => {
+    const lastChar = name.charCodeAt(name.length - 1);
+
+    if (lastChar >= 0xAC00 && lastChar <= 0xD7A3) {
+      const consonantCode = (lastChar - 44032) % 28;
+      return consonantCode === 0 ? '의' : '이의';
+    }
+    return '의';
+  };
+
+  const consonant = getConsonant(username);
+
   return (
     <div className='header-container'>
-      <label className='user-text'>{username}이의 편지함</label>
+      <label className='user-text'>{username}{consonant} 편지함</label>
     </div>
   );
 }
