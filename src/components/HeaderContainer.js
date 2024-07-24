7b003c3220f21048d6507273f9b8330d700001ce
JuyeonO <yexnx01@gmail.com>
2024-07-24 13:51:20 +0900

    fear: text UI 추가

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -2,5 +2,10 @@ import React from "react";
 import "../css/HeaderContainer.css";
 
 export default function HeaderContainer({ children }) {
-  return <div className="header-container">{children}</div>;
+  return (
+    <div className="header-container">
+      {children}
+      <label className="user-text">{}이의 편지함</label>
+    </div>
+  );
 }
