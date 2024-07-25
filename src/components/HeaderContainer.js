e351eeb0244c375396dd1edf4cb6ef936c46bc1d
JuyeonO <yexnx01@gmail.com>
2024-07-25 16:29:52 +0900

    feat: 홈 버튼 구현

--- src/components/HeaderContainer.js
+++ src/components/HeaderContainer.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import '../css/HeaderContainer.css';
 import getConsonant from '../utils/GetConsonant';
+import { useNavigate, useParams } from 'react-router-dom';
 
 export default function HeaderContainer({ name = '' }) {
   const [consonant, setConsonant] = useState();
+  const navigate = useNavigate();
+  const {memberId} = useParams();
 
   useEffect(() => {
     setConsonant(getConsonant(name));
   })
 
+  const goToMypage= () => {
+    const currentPath = window.location.pathname;
+
+    if (currentPath.includes('cloudy')) {
+      navigate('/cloudy/${memberId}') //로그인하지 않은 경우
+    }
+    else {
+      navigate(`/${memberId}`); //로그인 한 경우
+    }
+  }
+
   return (
     <div className='header-container'>
+      <button className='home-btn' onClick={goToMypage}>
+        <img src={require('../assets/img/Home.png')} alt=''/>
+      </button>
       <label className='user-text'>{name}{consonant} 편지함</label>
     </div>
   );
