b8a9009d6c47cc75825002d73f3fe8751d0fad70
JuyeonO <yexnx01@gmail.com>
2024-07-25 14:28:19 +0900

    feat: 로그인 플로팅 버튼 구현

--- src/components/FloatingBtn.js
+++ src/components/FloatingBtn.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import '../css/FloatingBtn.css';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 
 export default function FloatingBtn({ }) {
+    const location = useLocation();
     const navigate = useNavigate();
+    const {memberId} = useParams();
+    
     const onClickBtn = () => {
-        navigate('/cloudy/letter');
-    }
+        if (location.pathname.includes('cloudy')) {
+            navigate(`/cloudy/letter/member/${memberId}`); // 로그인하지 않은 경우 
+        } else {
+            navigate(`/settings/${memberId}`); // /로그인한 경우
+        }
+    };
 
     return (
       <button className='floating-button' onClick={(onClickBtn)}>
-        <img src={require('../assets/img/sunny.png')} alt='' />  
+        <img 
+                src={location.pathname.includes('cloudy') 
+                    ? require('../assets/img/sunny.png') // 로그인 전 플로팅 버튼
+                    : require('../assets/img/pen.png') // 로그인 후 플로팅 버튼
+                } 
+                alt='' 
+        />
       </button>
     );
 }
