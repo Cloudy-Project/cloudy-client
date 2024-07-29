a4cc21a09f3588b3f12ad3433c5038cbaa42e0c5
ywonchae1 <ywonchae62@gmail.com>
2024-07-29 20:38:39 +0900

    fix: onClickSubmit 두 번 호출되는 문제

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import '../css/Button.css';
 
 export default function Button({onClickBtn, btnName}) {
   return (
-    <button className='cloudy-button' onClick={() => onClickBtn()}>
+    <button type='button' className='cloudy-button' onClick={() => onClickBtn()}>
       {btnName}
     </button>
   )
